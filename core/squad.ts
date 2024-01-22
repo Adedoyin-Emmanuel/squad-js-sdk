f309f8759b1fbd73b76efc972ceeed48a1759471
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-01-22 11:00:51 +0100

    Feat: Finished SquadClient

--- core/squad.ts
+++ core/squad.ts
@@ -1,12 +1,15 @@
+import axios, { Axios } from "axios";
+
 /**
  * @summary
  * Squad JavaScript SDK
  * Built with ❣️ by Adedoyin Emmanuel Adeniyi https://github.com/adedoyin-emmanuel/
  */
-
 class SquadClient {
   private pubilcKey: string;
   private privateKey: string;
+  private Axios: Axios;
+  private baseUrl: string;
 
   /**
    * @summary
@@ -14,6 +17,12 @@ class SquadClient {
    */
   private environment: string = "development";
 
+  /**
+   * @summary This creates a new instance of Squad JS SDK
+   * @param {string} publicKey - Squad public key
+   * @param {string} privateKey - Squad private key
+   * @param {string} environment - The environment to use for the client. If not specified, defaults to "development".
+   */
   constructor(
     publicKey: string,
     privateKey: string,
@@ -27,5 +36,16 @@ class SquadClient {
     this.pubilcKey = publicKey;
     this.privateKey = privateKey;
     this.environment = environment;
+    this.baseUrl =
+      this.environment == "development"
+        ? "https://sandbox-api-d.squadco.com"
+        : "https://api-d.squadco.com";
+
+    this.Axios = axios.create({
+      baseURL: this.baseUrl,
+      headers: {
+        Authorization: `Bearer ${this.privateKey}`,
+      },
+    });
   }
 }
---
+++ helpers/axios.ts
@@ -0,0 +1,4 @@
+import axios from "axios";
+
+
+const Axios = axios.create({})
